3916e7aa8b57891807f11b010d50b92b4d11933f
Rob Ballantyne <raballantyne@gmail.com>
2019-07-11 20:15:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
-#def reverse_each_word(sentence)
- # sentence.split.each {|word| word.reverse}.join(" ")
-#end
-
 def reverse_each_word(sentence)
-  sentence = sentence.split(' ')
-  reversed = []
-  sentence.each do |word|
-    reversed.unshift(word)
-  end
-  return reversed.join(' ').reverse
+  sentence.split.each {|word| word.reverse}.join(" ")
 end
+
+#def reverse_each_word(sentence)
+ # sentence = sentence.split(' ')
+  #reversed = []
+  #sentence.each do |word|
+   # reversed.unshift(word)
+  #end
+  #return reversed.join(' ').reverse
+#end
